9b745c9449a4aedf9a34581b31b5c6c2aeb913b6
NoxiousInexperiencedModularity <73456427+Cat118@users.noreply.github.com>
2021-01-06 12:35:57 -0800

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -1,38 +1,35 @@
+// Global Variable Declartions
+const LOWER_CHAR_CODE = 97;
+const HIGHER_CHAR_CODE = 122;
+const ALPHABET_LENGTH = 26;
+
+const charPos = (char, shift = 0) => char.charCodeAt(0) + shift; // Converts character into it's key code plus shift
+
+const letterShift = (char, shift = 0, alphabetLength = 0) => String.fromCharCode(charPos(char, shift) + alphabetLength); // Converts charactor to string based on it's key code 
+
+const argsCriteria = (input , shift) => !input || !shift || shift === 0 || shift < -25 || shift > 25;// Checks to see if shift meets the correct criteria
 function caesar(input, shift, encode = true) {
-  if (!shift || shift > 25 || shift < -25) return false;
-  if (!input.length) return "";
-
-  // decoding changes the shift direction
-  if (!encode) shift = -shift;
-  input = input.toLowerCase();
-
-  let text = "";
-  for (let i = 0; i < input.length; i++) {
-    text += asciiMap(input.charCodeAt(i), shift);
-  }
-  return text;
-}
-
-function asciiMap(char, shift) {
-  // lower case ascii is from 97 to 122
-  const a = 97;
-  const z = 122;
-  // if what's passed in is something outside of this ascii range then just return it
-  if (char < a || char > z)  {
-    return String.fromCharCode(char);
-  } else {
-    if (char + shift < a) {
-      // if the char code falls below a loop around to z
-      // 96 should equal 122
-      return String.fromCharCode(z - ((a - (char + shift)) - 1));
-      // if the char code goes above z loop around to a
-      // 123 should equal 97
-    } else if (char + shift > z) {
-      return String.fromCharCode(a + ((char + shift) - z) - 1);
-    } else {
-      return String.fromCharCode(char + shift);
+    if (argsCriteria(input, shift)) { // Check's to see if we can run the full function
+        return false;
     }
-  }
+    input = input.toLowerCase(); // Changes our entire input into lowercase
+
+    !encode ? shift *= -1 : shift *= 1  // Intialize shift's positive/negative effects
+
+    let returnString = '';
+
+    for (const char of input) {
+        if (char.charCodeAt(0) < LOWER_CHAR_CODE || char.charCodeAt(0) > HIGHER_CHAR_CODE) { // Add special characters
+            returnString += char;
+        } else if (charPos(char, shift) < LOWER_CHAR_CODE) { // +++ Wraps letter around from a to z
+            returnString += letterShift(char, shift, ALPHABET_LENGTH)
+        } else if (charPos(char, shift) > HIGHER_CHAR_CODE) { // --- Wraps letter around from z to a
+            returnString += letterShift(char, shift, -ALPHABET_LENGTH)
+        } else { // return letter based on shift
+            returnString += letterShift(char, shift);
+        }
+    }
+    return returnString;
 }
 
-module.exports = { caesar, asciiMap };
+module.exports = caesar;
