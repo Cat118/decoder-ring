d69611240b33163c444f4a98457ffcd743d70231
NoxiousInexperiencedModularity <73456427+Cat118@users.noreply.github.com>
2021-01-06 12:33:25 -0800

    Update caesar.test.js

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,75 +1,53 @@
 const expect = require("chai").expect;
+const caesar = require("../src/caesar");
 
-const { caesar, asciiMap } = require("../src/caesar");
+describe("caesar", () => {
+    it("Should shift so that it goes off the alphabet (e.g. a shift of 3 on the letter z).", () => {
+        const expected = 'wklqnixo';
+        const actual = caesar("thinkful", 3 );
+        expect(actual).to.equal(expected);
+    });
 
-describe("caeser", () => {
-  it("returns false if shift value is missing", () => {
-    const actual = caesar();
-    expect(actual).to.be.false;
-  });
-  it("returns false if shift value is 0", () => {
-    const actual = caesar("", 0);
-    expect(actual).to.be.false;
-  });
-  it("returns false if shift value is greater than 25", () => {
-    const actual = caesar("", 26);
-    expect(actual).to.be.false;
-  });
-  it("returns false if shift value is less than -25", () => {
-    const actual = caesar("", -27);
-    expect(actual).to.be.false;
-  });
-  it("shifts a provided string by the shift amount", () => {
-    const expected = "ibm";
-    const actual = caesar("hal", 1);
-    expect(actual).to.equal(expected);
-  });
-  it("is case insensitive", () => {
-    const expected = "ibm";
-    const actual = caesar("HAL", 1);
-    expect(actual).to.equal(expected);
-  });
-  it("preserves whitespace", () => {
-    const expected = "qrgp rqf";
-    const actual = caesar("open pod", 2);
-    expect(actual).to.equal(expected);
-  });
-  it("preserves any nonalphabetic character", () => {
-    const expected = "@kruvh_herrnv";
-    const actual = caesar("@horse_ebooks", 3);
-    expect(actual).to.equal(expected);
-  });
-  it("handles left shifts beyond a", () => {
-    const expected = "xifyxyx";
-    const actual = caesar("alibaba", -3);
-    expect(actual).to.equal(expected);
-  });
-  it("handles shifts above z", () => {
-    const expected = "aoacss";
-    const actual = caesar("xlxzpp", 3);
-    expect(actual).to.equal(expected);
-  });
-  it("properly decodes a message", () => {
-    const expected = "this is a secret message!";
-    const actual = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
-    expect(actual).to.be.equal(expected);
-  });
-});
+    it("Should shift so that it goes off the alphabet a shift of -3 on the letter a).", () => {
+        const expected = 'qefkhcri'
+        const actual = caesar("thinkful", -3 );
+        expect(actual).to.equal(expected);
+    });
+    it("Should actually decodes.", () => {
+        const expected = 'thinkful';
+        const actual = caesar("wklqnixo", 3, false );
+        expect(actual).to.equal(expected);
+    });
 
-describe("asciiMap", () => {
-  it("returns a character code's string representation", () => {
-    const expected = "a";
-    const actual = asciiMap(97, 0);
-    expect(actual).to.equal(expected);
-  });
-  it("returns a when given z and a shift of 1", () => {
-    const expected = "a";
-    const actual = asciiMap(122, 1);
-    expect(actual).to.equal(expected);
-  });
-  it("returns z when given a and a shift of -1", () => {
-    const expected = "z";
-    const actual = asciiMap(97, -1);
-    expect(actual).to.equal(expected);
-  });
+    it("Should maintain spaces throughout, as should other non-alphabetic symbols.", () => {
+        const expected = 'bpqa !qa @i #amkzmb umaaiom!';
+        const actual = caesar('This !is @a #secret message!', 8);
+        expect(expected).to.equal(actual);
+    });
+    it("Should ignore Capital letters.", () => {
+        const expected = 'this is a secret message!';
+        const actual = caesar("BPQA qa I amkzmb umaaiom!", 8, false);
+        expect(actual).to.equal(expected);
+    });
+
+    it("Should return false if the shift value is not present or equal to 0.", () => {
+        const actual = caesar('thinkful');
+        expect(actual).to.be.false;
+    });
+    it("Should return false if the shift value is greater than 25.", () => {
+        const actual =caesar('thinkful', 99);
+        expect(actual).to.be.false;
+    });
+    it("Should return false if the shift is less than -25.", () => {
+        const actual = caesar('thinkful', -26);
+        expect(actual).to.be.false;
+    });
+    it("Should return false if shift is 0.", () => {
+        const actual = caesar('thinkful', 0);
+        expect(actual).to.be.false;
+    });
+    it('Should return false if input or shift value is missing', () => {
+        const actual = caesar()
+        expect(actual).to.be.false;
+    })
 })
